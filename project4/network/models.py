17843858a24764ceb7d656a45baf32b11b63aedc
Bastian Elfert <bastian.elfert@outlook.com>
2020-11-17 17:10:37 +0200

    creating models without relationships

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    username = models.CharField(
+        max_length=64,
+        null=False
+    )
+    # followers
+    # following
+    # posts)
+
+# class Post(models.Model):
+#     pass
+    # user
+    # content
+    # creation_datetime
+    # likes
+
+    # def __str__(self):
+    #     return f"Listing: {self.listing} | User: {self.user}"
+
+    # def serialize(self):
+    # return {
+    #     "id": self.id,
+    #     "sender": self.sender.email,
+    # }
