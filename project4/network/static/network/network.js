83df894afba5a1bf6225cfb8d39644990ea36efa
Bastian Elfert <bastian.elfert@outlook.com>
2020-11-18 15:49:44 +0200

    added network.js, added script to layout/index. added form for posting to index.html.

---
+++ project4/network/static/network/network.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#compose-form').addEventListener('submit', (event) => {
+        event.preventDefault();
+        send_post();
+      });
+});
+
+async function send_post(){
+    const response = await fetch('/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        op: document.querySelector('#compose-op').value,
+        content: document.querySelector('#compose-body').value
+    })
+    });
+    const json = await response.json();
+    load_mailbox('sent');
+}
